399360df4ef82d8de91705557b25b5dce0dee2aa
ecarry <lianshiliang93@gmail.com>
2026-02-14 15:33:32 +0000

    Add CodeMirror editor with JavaScript/TypeScript support and display logo when no file is active in EditorView

---
+++ src/features/editor/components/code-editor.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import { basicSetup, EditorView } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+export const CodeEditor = () => {
+  const editorRef = useRef<HTMLDivElement>(null);
+  const viewRef = useRef<EditorView | null>(null);
+
+  useEffect(() => {
+    if (!editorRef.current) return;
+
+    const view = new EditorView({
+      doc: "Start document",
+      parent: editorRef.current,
+      extensions: [
+        basicSetup,
+        javascript({
+          typescript: true,
+        }),
+      ],
+    });
+
+    viewRef.current = view;
+
+    return () => {
+      view.destroy();
+    };
+  }, []);
+
+  return <div ref={editorRef} className="size-full pl-4 bg-background" />;
+};
--- src/features/editor/components/editor-view.tsx
+++ src/features/editor/components/editor-view.tsx
@@ -2,15 +2,36 @@ import { Id } from "../../../../convex/_generated/dataModel";
 import { TopNavigation } from "./top-navigation";
 import { useEditor } from "../hooks/use-editor";
 import { FileBreadcrumbs } from "./file-breadcrumbs";
+import { useFile } from "@/features/projects/hooks/use-files";
+import Image from "next/image";
+import { CodeEditor } from "./code-editor";
 
 export const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
   const { activeTabId } = useEditor(projectId);
+  const activeFile = useFile(activeTabId);
+
   return (
     <div className="h-full flex flex-col">
       <div className="flex items-center">
         <TopNavigation projectId={projectId} />
       </div>
       {activeTabId && <FileBreadcrumbs projectId={projectId} />}
+
+      <div className="flex-1 min-h-0 bg-background">
+        {!activeFile && (
+          <div className="size-full flex items-center justify-center">
+            <Image
+              src="/logo-alt.svg"
+              alt="Polaris"
+              width={50}
+              height={50}
+              className="opacity-25"
+            />
+          </div>
+        )}
+
+        {activeFile && <CodeEditor />}
+      </div>
     </div>
   );
 };
